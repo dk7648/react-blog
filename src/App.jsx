9e7713301f2a60b4b064b4fe5b737b09940f054f
dk7648 <dk7648@korea.ac.kr>
2025-03-06 15:26:06 +0900

    part1: 자식이 부모의 state 가져다쓰고 싶을 때는 props

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,62 @@
 /* esLint-disable */
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let post = '강남 우동 맛집';
-  let [title, setTitle] = useState(['남자 코트 추천','강남 우동 맛집', '파이썬독학']);
-  let [like, setLike] = useState([0,0,0]);
-  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(["남자 코트 추천", "강남 우동 맛집", "파이썬독학"]);
+  let [like, setLike] = useState([0, 0, 0]);
+  let [modal, setModal] = useState([false, false, false]);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>BLOG</h4>
       </div>
-    {
-      title.map(function(t, index) {
+      {title.map(function (t, index) {
         return (
-        <div className="list" key={index}>
-          <h4 onClick={()=> {setModal(!modal)}}> {index+1}: {t} 
-            <span onClick={()=>{ 
-              let copy = [...like]
-              copy[index] += 1
-              setLike(copy)}}>❤️
-            </span> {like[index]} 
-          </h4>
-          <p>2월 17일 발행</p>
-        </div>
+          <div className="list" key={index}>
+            <h4
+              onClick={() => {
+                let copy = [...modal];
+                copy[index] = !copy[index];
+                setModal(copy);
+              }}
+            >
+              {" "}
+              {index + 1}: {t}
+              <span
+                onClick={() => {
+                  let copy = [...like];
+                  copy[index] += 1;
+                  setLike(copy);
+                }}
+              >
+                ❤️
+              </span>{" "}
+              {like[index]}
+            </h4>
+            <p>2월 17일 발행</p>
+            {modal[index] ? <Modal title={title} setTitle={setTitle} i={index} /> : null}
+          </div>
         );
-      })
-      
-    }
-      <div className="list">
-          <h4>{title[1]}</h4>
-          <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-          <h4 onClick={()=> {setModal(!modal)}}>{title[2]}</h4>
-          <p>2월 17일 발행</p>
-      </div>
-    <button onClick={()=>{
-      let newTitle = [...title];
-      newTitle[0] = '여자 코트 추천';
-      setTitle(newTitle);}}
-      >코트 변경</button>
-
-      <button onClick={() => {
-        let newTitle = [...title];
-        newTitle.sort();
-        setTitle(newTitle);
-      }}>정렬</button>
-      <h4>{ post }</h4>
-      {
-        modal ? <Modal/> : null
-      }
-
-      
-      
-      
-    </div>
-  )
-}
-
-function Modal() {
-  return (
-    <div className="modal">
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
+      })}
     </div>
   );
 }
 
-export default App
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h4>{props.title[props.i]}</h4>
+      <p>{props.date}</p>
+      <p>{props.content}</p>
+      <button onClick={()=>{
+        let copy = [...props.title]
+        copy[props.i] = "여자 코트 추천"
+        props.setTitle(copy)
+      }}>글수정</button>
+    </div>
+  );
+}
+
+export default App;
